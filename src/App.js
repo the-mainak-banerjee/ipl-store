54cb719d6cfa795ac579349cc36e6330fdd7e889
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-13 15:19:03 +0530

    created homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from "./components";
-import { ProductPage } from "./pages";
+import { HomePage, ProductPage } from "./pages";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routes>
+        <Route path='/' element={<HomePage/>}/>
         <Route path='/products' element={<ProductPage/>}/>
       </Routes>
       <Footer/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
+export { HomePage } from './home-page/HomePage'
 export { ProductPage } from './product-pages/ProductPage'

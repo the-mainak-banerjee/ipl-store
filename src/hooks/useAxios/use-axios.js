c141d1fad6fd60c0af29ad34a1891356dd1edc3f
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-13 13:31:19 +0530

    installed axios and created a custom hook for that

---
+++ src/hooks/useAxios/use-axios.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { useState } from "react"
+
+const useAxios = () => {
+    const [response,setResponse] = useState(undefined)
+    const [error, setError] = useState('')
+    const [loading,setLoading] = useState(false)
+
+    const operation = async (config) => {
+        try{
+            setLoading(true)
+            const results = await axios.request(config)
+            setResponse(results.data)
+        }catch(error){
+            setError(error)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    return { operation, response, error, loading}
+}

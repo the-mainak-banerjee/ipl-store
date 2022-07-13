8f199796a1efcbee92f9982b6c0119dca7c71aed
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-13 15:36:45 +0530

    created productdetailspage

--- src/hooks/usePageTitle/use-pageTitle.js
+++ src/hooks/usePageTitle/use-pageTitle.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react"
 
 export const usePageTitle = (title) => {
     const [pageTitle, setPageTitle] = useState(title)
+    console.log(title)
+    console.log(pageTitle)
     useEffect(() => {
+        setPageTitle(title)
         document.title = pageTitle
-    },[pageTitle])
+    },[title, pageTitle])
 
     return { pageTitle, setPageTitle }
 }
